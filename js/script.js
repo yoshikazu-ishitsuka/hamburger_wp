5e5b7900b862954f744d967e8869e9aa8edf3903
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2022-11-13 20:00:08 +0900

    sidebarと各ファイルの編集

--- js/script.js
+++ js/script.js
@@ -1,15 +1,36 @@
 // headerのMenuボタン押下時の処理
 $(function () {
   $(".c-button--menu.js-button").click(function () {
-    $(".l-sidebar").show();
+    // $(".l-sidebar").show();
+    $(".l-sidebar").css({
+      visibility: "visible",
+      left: "0",
+      transition: "0.5s",
+      // position: "fixed",
+      // position: "sticky",
+      // top: 0,
+    });
     $(".l-sidebar").toggleClass(".is-open");
+    // $(".l-sidebar.js-trigger.is-open").css({ position: "sticky", top: "0" });
   });
 });
+
+// $(function () {
+//   $(".c-button--menu.js-button").click(function () {
+//     // $("l-sidebar.js-trigger.is-open").css({ position: "sticky", top: "0" });
+//     $("l-sidebar").css({ position: "sticky", top: "0" });
+//   });
+// });
 
 // sidebarのXボタン押下時の処理
 $(function () {
   $(".menu-close.js-button").click(function () {
-    $(".l-sidebar").hide();
+    // $(".l-sidebar").hide();
     $(".l-sidebar").removeClass(".is-open");
+    $(".l-sidebar").css({
+      // visibility: "hidden",
+      left: "23.4375rem",
+      transition: "0.5s",
+    });
   });
 });
