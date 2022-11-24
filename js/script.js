2a08e2c58d9ae61f991c935d6c57742febd35367
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2022-11-24 20:23:24 +0900

    js/内のjQueryの編集

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ $(function () {
       left: "0",
       "transition-duration": "0.5s",
     });
-    $(".side-wrapper").toggleClass("is-open");
+    // $(".side-wrapper").toggleClass("is-open");
     $(".l-sidebar__main").toggleClass("is-open");
     $("body").css({ position: "fixed", overflow: "scroll" });
     $(".wrapper").css({ position: "sticky", top: "0" });
@@ -21,7 +21,7 @@ $(function () {
   $(".menu-close.js-button").click(function () {
     $(".u-background--overlay").toggleClass("js-height");
     $(".l-sidebar__main").removeClass("is-open");
-    $(".side-wrapper").removeClass("is-open");
+    // $(".side-wrapper").removeClass("is-open");
     $(".side-wrapper").css({
       "z-index": "-1",
       left: "100%",
@@ -29,4 +29,18 @@ $(function () {
     $(".wrapper").css({ position: "static" });
     $("body").css({ position: "static", overflow: "inherit" });
   });
+});
+
+//サイドバー表示中にブラウザをレスポンシブした時の処理（ｘボタン押下時と同じスタイルを適用）
+$(window).resize(function () {
+  if (window.matchMedia("(min-width: 1025px)").matches) {
+    $(".l-sidebar__main").removeClass("is-open");
+    $(".u-background--overlay").removeClass("js-height");
+    $(".wrapper").css({ position: "static" });
+    $("body").css({ position: "static", overflow: "inherit" });
+    $(".side-wrapper").css({
+      "z-index": "-1",
+      left: "100%",
+    });
+  }
 });
